457ef9d7289da19ab7e6da36079b18bff43688e5
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-07 14:27:05 +0200

    01.1

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,20 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+function log_first_letter(array) {
+    
+    for (let i = 0; i < array.length; i++) {
+        const first_letter = array[i][0];
+        console.log(first_letter);
+    }
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+log_first_letter(names);
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
