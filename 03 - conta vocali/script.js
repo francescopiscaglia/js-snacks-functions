036669a63ce29cc3a21f8c3865952a71ded1a278
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-07 15:07:33 +0200

    03

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,25 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function number_of_vocals(string) {
 
+    let vowals = ["a", "e", "i", "o", "u",];
+    let vowals_count = 0;
+    
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i];
+
+        if (vowals.includes(element)) {
+            vowals_count++;
+            console.log(element);
+        } 
+    }
+    
+    console.log(vowals_count);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+number_of_vocals(word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
